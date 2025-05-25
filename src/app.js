e4ad80420b176c10d2a0f6b459d5d4e6d57f8711
DiegoAmorin33 <amorinoficial33@gmail.com>
2025-05-25 11:53:26 +0000

    excusegeneratordone

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function randomizenumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomizearray(arrayRandom) {
+  let max = arrayRandom.length - 1;
+  let min = 0;
+  let random = randomizenumber(min, max);
+  return arrayRandom[random]
+}
+
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = randomizearray(who) + " " + randomizearray(action) + " " + randomizearray(what) + " " + randomizearray(when)
 };
